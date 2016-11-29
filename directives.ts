ce4150eeae9b08fe035218d3a7c70dabd0b4042b
byzg <byzg@mail.ru>
2016-11-29 09:31:53 +0300

    gulp

--- directives.ts
+++ directives.ts
@@ -1,7 +1,6 @@
 import { MultiPicker } from './src/components/multi-picker/multi-picker';
 
 export * from './src/components/multi-picker/multi-picker';
-export * from './src/components/multi-picker/multi-picker-columns';
 
 export const MULTI_PICKER_DIRECTIVES = [
     MultiPicker
